67ca6a15c18701489241110f004a90dc69124f8e
Edgar Huaranga <edgarj.huaranga@gmail.com>
2023-12-21 13:33:22 -0500

    fenomenos

--- src/components/AtlasMap/AtlasMap.jsx
+++ src/components/AtlasMap/AtlasMap.jsx
@@ -23,23 +23,23 @@ const svgIcon = L.divIcon({
   iconAnchor: [10, 10]
 });
 
-function filterMap(word, mapstyle){
-  
+function filterMap(word, mapstyle) {
+
   let result = words.filter((w) => w.word === word)[0];
-  if(mapstyle === "fenomeno"){
+  if (mapstyle === "fenomeno") {
     result = phenomenos.filter((w) => w.key === word)[0];
   }
-  console.log(result) 
-  console.log(result['distribution']);
 
   var filteredMap = andalucia.features.filter((feature) => {
-    for(var i=0; i<result.distribution.length; i++){
+    for (var i = 0; i < result.distribution.length; i++) {
       let pc = result.distribution[i].postalcode;
-      if(feature.properties.name === pc){
+      if (feature.properties.name === pc) {
         feature.properties["variation"] = result.distribution[i].variation
         feature.properties["audioURL"] = result.distribution[i].audioURL
         feature.properties["comment"] = result.distribution[i].comment
-        feature.properties["frequency"] = result.distribution[i].frequency
+        feature.properties["category"] = result.distribution[i].category
+        let cat = result.distribution[i].category;
+        feature.properties["color"] = result.variations.filter((c) => c.type === cat)[0].color
         return feature
       }
     }
@@ -48,11 +48,50 @@ function filterMap(word, mapstyle){
   return filteredMap;
 }
 
-function Leyenda(titulo) {
+function Leyenda(titulo, mapstyle) {
+
+  let legend = andalucia.features.map((object) => {
+    return { "color": object.properties.color, "label": object.properties.category }
+  })
+
+  const uniqueIds = [];
+  const unique = legend.filter(element => {
+    const isDuplicate = uniqueIds.includes(element.label);
+    if (!isDuplicate) {
+      uniqueIds.push(element.label);
+      return true;
+    }
+    return false;
+  });
+
+
+  var table = <></>
+
+  if (mapstyle === "fenomeno") {
+    table = <table>
+      {
+        unique?.map((item, key) => {
+          return (
+            <tr key={key}>
+              <td style={{ backgroundColor: item.color }}>&nbsp;</td>
+              <td>{item.label}</td>
+            </tr>
+          )
+
+        }
+        )}
+    </table>
+  }
+
+
+
+
+
   return (
     <Card elevation={2}>
       <CardContent>
         <Typography variant="h1">{titulo}</Typography>
+        {table}
       </CardContent>
     </Card>
   )
@@ -60,6 +99,7 @@ function Leyenda(titulo) {
 
 function MyComponent() {
   let { word } = useParams();
+  let {mapstyle} = useParams();
   const map = useMapEvents({
     click: () => {
       //console.log('map center:', map.getCenter())
@@ -77,7 +117,7 @@ function MyComponent() {
     if (legends.length == 0) {
       legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', styles.legend);
-        div.innerHTML = ReactDOMServer.renderToString(Leyenda(word));
+        div.innerHTML = ReactDOMServer.renderToString(Leyenda(word, mapstyle));
         return div;
       };
       legend.addTo(map);
@@ -99,44 +139,44 @@ const Map = ({ polygons, data, setPostalCodeClicked }) => {
 
   const finalData = filterMap(word, mapstyle);
   andalucia.features = finalData;
-  
+
   if (mapstyle === "palabra") {
-    var secondLayer = 
-    <>
-    <GeoJSON
-      onEachFeature={(feature, layer) => {
-        layer.options.fillColor = "#006E39"
-        layer.options.color = "black"
-        layer.options.fillOpacity = 0.8
-        layer.options.weight = 1
-        layer.options.dashArray = 1
-        layer.options.opacity = 1
+    var secondLayer =
+      <>
+        <GeoJSON
+          onEachFeature={(feature, layer) => {
+            layer.options.fillColor = "#006E39"
+            layer.options.color = "black"
+            layer.options.fillOpacity = 0.8
+            layer.options.weight = 1
+            layer.options.dashArray = 1
+            layer.options.opacity = 1
 
-        layer.on({
-          click: (event) => {
-            const districtName = event.target.feature.properties.comunidad
-            console.log(districtName + " - " + layer.feature.properties.lugar);
-            setPostalCodeClicked(layer);
-          }
-        })
-
-
-        let popupContent = feature.properties.variation
-        let tooltipOptions = { permanent: true, opacity: 0.75, className: styles.leaflet_tooltip }
-        layer.bindTooltip(popupContent, tooltipOptions);
+            layer.on({
+              click: (event) => {
+                const districtName = event.target.feature.properties.comunidad
+                console.log(districtName + " - " + layer.feature.properties.lugar);
+                setPostalCodeClicked(layer);
+              }
+            })
 
 
-      }}
-      key={1} data={JSON.parse(JSON.stringify(andalucia))} style={{ weight: 1 }} />
-      
-    </>
+            let popupContent = feature.properties.variation
+            let tooltipOptions = { permanent: true, opacity: 0.75, className: styles.leaflet_tooltip }
+            layer.bindTooltip(popupContent, tooltipOptions);
+
+
+          }}
+          key={1} data={JSON.parse(JSON.stringify(andalucia))} style={{ weight: 1 }} />
+
+      </>
   }
 
   else if (mapstyle === "fenomeno") {
-    
+
     secondLayer = <GeoJSON
       onEachFeature={(feature, layer) => {
-        layer.options.fillColor = colors[Math.floor(Math.random() * colors.length)]
+        layer.options.fillColor = feature.properties.color
         layer.options.color = "black"
         layer.options.fillOpacity = 0.5
         layer.options.weight = 1
@@ -153,7 +193,7 @@ const Map = ({ polygons, data, setPostalCodeClicked }) => {
 
       }}
       key={1} data={JSON.parse(JSON.stringify(andalucia))} style={{ weight: 1 }} />
-      
+
   }
 
 
@@ -171,7 +211,7 @@ const Map = ({ polygons, data, setPostalCodeClicked }) => {
           }} key={2} data={JSON.parse(JSON.stringify(spania))} />
 
         {secondLayer}
-       
+
         <MyComponent />
 
       </MapContainer>
